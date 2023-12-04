661af4a99727ce890d4a9164a25c27a0228627bc
Clemens Brunner <clemens.brunner@gmail.com>
2023-12-04 14:48:16 +0100

    Use more pipes

--- day04_1.jl
+++ day04_1.jl
@@ -3,13 +3,14 @@ include("aoc.jl")
 cookie = ""
 input = get_aoc_input(4, cookie)
 
-function get_points(card)
+function matches(card)
     winning = parse.(Int, split(strip(chopprefix(card[1], r"Card +\d+:"))))
     numbers = parse.(Int, split(strip(card[2])))
-    matches = length(winning ∩ numbers)
-    return matches > 0 ? 2^(matches - 1) : 0
+    return length(winning ∩ numbers)
 end
+
+points(matches) = matches > 0 ? 2^(matches - 1) : 0
 
 cards = input |> strip |> x -> split(x, "\n") |> x -> split.(x, "|")
 
-println("Part 1: ", sum(get_points.(cards)))
+println("Part 1: ", cards .|> matches .|> points |> sum)
